71a4e899a71b3ee8e8a0f0e7cf1a783badb9948b
Gregory Meiste <greg.meiste@gmail.com>
2022-10-02 17:35:40 -0500

    Enable rm_work for all and ENABLE_UART for rpi

--- run-build.sh
+++ run-build.sh
@@ -3,6 +3,9 @@
 # Abort script if any command returns error
 set -e
 
+IMAGE="core-image-base"
+TMP_FILE=".env_check"
+
 # Source build environment
 . ./poky/oe-init-build-env build
 
@@ -10,5 +13,15 @@ set -e
 bitbake-layers add-layer ../poky/meta-raspberrypi
 bitbake-layers add-layer ../poky/meta-intel
 
+# Parse environment
+bitbake -e $IMAGE > $TMP_FILE
+TARGET=`grep ^MACHINE= $TMP_FILE | cut -d\" -f2`
+rm $TMP_FILE
+
+# Machine-specific variables
+if [ "$TARGET" == "raspberrypi3-64" ]; then
+  export ENABLE_UART=1
+fi
+
 # Build
-bitbake core-image-base
+bitbake $IMAGE
